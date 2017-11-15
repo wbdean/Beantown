cd9bc5c7fd7241a742cd8c9ea0c87f265452b9d5
wbdean <swilliam127@gmail.com>
2017-11-15 17:43:06 -0500

    Look at reviews

--- Text_Analysis.R
+++ Text_Analysis.R
@@ -1,3 +1,20 @@
 # William Dean
 # Text and Emoji Analysis of Airbnb and Twitter Data
 
+## Load Data --------------------
+
+# Air bnb
+bostonreview <- read.csv("boston/reviews.csv")
+bostonlisting <- read.csv("boston/listings.csv")
+load("Boston.Rda")
+chicagolisting <- read.csv("chicago/listings.csv")
+chicagoreview <- read.csv("chicago/reviews.csv")
+load("Chicago.Rda")
+load("Cambridge.Rda")
+
+## Text -------------------------------------
+
+ascii <- function(x) {
+    # Convert tex to ascii 
+    iconv(x, from = "latin1", to = "ascii", sub = "byte") 
+}
